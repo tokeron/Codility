9b213e4604cf1b7690ebe7583fa486bdb0c6c02e
Michael Toker <49562866+tokeron@users.noreply.github.com>
2020-03-14 17:22:23 +0200

    Create StrSymmetryPoint.cpp
    https://app.codility.com/programmers/lessons/99-future_training/array_inversion_count/

---
+++ StrSymmetryPoint.cpp
@@ -0,0 +1,20 @@
+//Codility: Stacks and Queues
+//Task: https://app.codility.com/programmers/lessons/99-future_training/array_inversion_count/
+
+
+#include <ostream>
+using std::string;
+
+
+
+int solution(string &S) {
+    int N = S.length();
+    if ((N == 0) || (N % 2 == 0)) return -1;
+    if (N == 1) return 0;
+    for (int i = 0; i < (N + 1) / 2; i++) {
+        if (S[i] != S[N - 1 - i]) {
+            return -1;
+        }
+    }
+    return (N + 1) / 2;
+}
